712565125b0eeabb63eebac02d118a8889af8790
송현민 <ondskan@naver.com>
2022-09-29 19:37:38 +0900

    초기 커밋.

---
+++ 0929.c/idiom.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+#define IDIOM "The Cosmos is all that is or ever was or ever will be. - %d Carl Sagon"
+
+int main()
+
+{
+
+	printf("%s\b\b\b\b\b\b\b\b\b\b\b\b\b\b1980 Carl Sagon ",IDIOM);
+
+	return 0;
+}
+/*e.g., 
+
+The Cosmos is all that is or ever was or ever will be. - 1980 Carl Sagon
+
+
+
+
+*/
+
